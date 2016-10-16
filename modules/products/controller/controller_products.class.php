9e856bd11021eab2b7a611ff0683421a3fba7b71
Oscar Otero <oscar.otero.millan@gmail.com>
2016-10-16 09:32:09 +0200

    Create & refactor model/*php, products/model/, utils/common.inc.php

--- modules/products/controller/controller_products.class.php
+++ modules/products/controller/controller_products.class.php
@@ -31,6 +31,7 @@ function discharge_products() {
 
     	$arrArgument = array(
             'product_name' => ucfirst($result['data']['product_name']),
+            'price' => $result['data']['price'],
             'description' => $result['data']['description'],
             'discharge_date' => $result['data']['discharge_date'],
             'expiry_date' => $result['data']['expiry_date'],
@@ -38,7 +39,6 @@ function discharge_products() {
             'provider_phone' => $result['data']['provider_phone'],
             'season' => $result['data']['season'],
             'category' => $result['data']['category'],
-            'price' => $result['data']['price'],
 
             'avatar' => $result_avatar['data']
         );
---
+++ modules/products/model/DAO/product_dao.class.singleton.php
@@ -0,0 +1,42 @@
+<?php
+class productDAO {
+
+    static $_instance;
+
+    private function __construct() {
+
+    }
+
+    public static function getInstance() {
+        if (!(self::$_instance instanceof self))
+            self::$_instance = new self();
+        return self::$_instance;
+    }
+
+    public function create_product_DAO($db, $arrArgument) {
+        $product_name = $arrArgument['product_name'];
+        $price = $arrArgument['price'];
+        $description = $arrArgument['description'];
+        $discharge_date = $arrArgument['discharge_date'];
+        $expiry_date = $arrArgument['expiry_date'];
+        $provider_email = $arrArgument['provider_email'];
+        $provider_phone = $arrArgument['provider_phone'];
+        $season = $arrArgument['season'];
+
+        $avatar = $arrArgument['avatar'];
+
+        $category = $arrArgument['category'];
+        $str_category = "";
+
+        foreach ($category as $indice) {
+            $str_category += $indice +":";
+        }
+
+        $sql = "INSERT INTO products(`product_name`, `price`, `description`, `discharge_date`, `expiry_date`, `provider_email`, `provider_phone`, `season`, `categorie`, `avatar` )"
+                . " VALUES ('$product_name', '$price', '$description',"
+                . " '$discharge_date', '$expiry_date', '$provider_email', '$provider_phone', '$season', '$category', '$avatar')";
+
+        return $db->ejecutar($sql);
+    }
+
+}
