3ce7072743e60b50f57ca3ddd38779ca9de44ae0
Oscar Otero <oscar.otero.millan@gmail.com>
2016-11-03 17:50:22 +0100

    Refactor list_products.js, list_products.php

--- modules/products_FE/view/list_products.php
+++ modules/products_FE/view/list_products.php
@@ -3,38 +3,41 @@
 <div id="results"></div>
 
 <center>
-    <div class="pagination"></div>
+<form name="search_prod" id="search_prod" class="search_prod">
+    <input type="text" value="" placeholder="Search Product ..." class="input_search" id="keyword" list="datalist">
+    <!-- <div id="results_keyword"></div> -->
+    <input name="Submit" id="Submit" class="button_search" type="button" />
+
+</form>
 </center>
 
-<!-- modal window details_product -->
-<section id="product">
+<div id="results"></div>
 
-    <div id="details_prod" hidden>
+<center>
+    <div class="pagination_prods"></div>
+</center>
 
-        <!--<ol class="breadcrumb">
-            <li><a href="index.php?module=products">Products</a></li>
-            <li class="active">Details Product</li>
-        </ol>
-        <br>
-        <br>-->
-        <div id="details">
-            <div id="img_prod" class="prodImg"></div>
+<!-- modal window  -->
 
-            <div id="container">
+<section >
+    <div class="container" id="product">
 
-                <h4> <strong><div id="name_prod"></div></strong> </h4>
-                <br />
-                <p>
-                <div id="description_prod"></div>
-                </p>
-                <p>
-                <div id="titration_prod"></div>
-                </p>
-                <h2> <strong><div id="price_prod"></div></strong> </h5>
+
+        <div class="media">
+            <div class="pull-left">
+                <div id="img_product" class="img_product"></div>
+            </div>
+            <div class="media-body">
+                <div id="text-product">
+                <h3 class="media-heading title-product"  id="nom_product"></h3>
+                <p class="text-limited" id="desc_product" ></p>
+                <br>
+                <h5 > <strong  id="price_product"></strong> </h5>
+                </div>
 
             </div>
-
         </div>
+
 
     </div>
 </section>
